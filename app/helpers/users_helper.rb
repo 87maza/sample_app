52084a7988ce89fee2ac0dfba8b3ae567c4f5154
Martin Maza <Martin@Martins-MacBook-Pro.local>
2015-05-25 18:11:47 -0700

    finished sigup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = "d2f805cc927b8c978945adaa282e108b"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
